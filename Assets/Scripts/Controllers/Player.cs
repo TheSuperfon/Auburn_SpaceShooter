720032bf06d5f462e86de4a8a3c3abf74423c39e
TheSuperfon <astefanadon@gmail.com>
2024-09-17 14:09:45 -0400

    create player movement
    player can use WSAD to move the player ship

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,10 +8,38 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public float speed = 0.2f;
 
     void Update()
     {
-
+        
     }
+
+    private void FixedUpdate()
+    {
+        playermovementMethod();
+        
+    }
+    void playermovementMethod()
+    {
+        //transform.position += direction;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += (Vector3.up * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += (Vector3.down * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += (Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += (Vector3.right * speed);
+        }
+    }
+
 
 }
