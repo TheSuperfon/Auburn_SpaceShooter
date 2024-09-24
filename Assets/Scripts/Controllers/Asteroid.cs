71876623e5206d56173c494c1b8fa6a05c1eca6e
TheSuperfon <astefanadon@gmail.com>
2024-09-23 21:18:49 -0400

    Task 3 to the best of my abilities
    asteroid movement (though maybe speed is a bit much, could make it variable for future reference)

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 3f;
     public float arrivalDistance;
-    public float maxFloatDistance;
+    public float maxFloatDistanceX;
+    public float maxFloatDistanceY;
+    public Vector3 Newposition;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        maxFloatDistanceX = Random.Range(-5,6);
+        maxFloatDistanceY = Random.Range(-5, 6);
     }
 
     // Update is called once per frame
@@ -20,13 +25,16 @@ public class Asteroid : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AsteroidMovement();
         
     }
 
 
-    public void EnemyMovement()
+    public void AsteroidMovement()
     {
-
+        //Newposition = new Vector3(transform.position.x + maxFloatDistance,transform.position.y + maxFloatDistance,0);
+        Newposition = new Vector3(transform.position.x + maxFloatDistanceX, transform.position.y + maxFloatDistanceY, 0);
+        transform.position = Vector3.Lerp(transform.position, Newposition, moveSpeed);
     }
 
 }
