5e5eb3fbae0f221146a3559604a98060139e1f2d
TheSuperfon <astefanadon@gmail.com>
2024-09-23 21:46:23 -0400

    Task 4a to the best of my abilities
    Constellation drawn but all at once.

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -6,9 +6,29 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    public float timer = 0f;
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    private void FixedUpdate()
+    {
+        DrawConstellations();
+    }
+
+
+    public void DrawConstellations()
+    {
+        for (int i = 0; i < starTransforms.Count - 1; ++i)
+        {
+            Debug.DrawLine(starTransforms[i].position, starTransforms[i + 1].position, Color.white);
+            if (timer < 3)
+            {
+                timer += Time.deltaTime;
+            }
+            timer = 0f;
+        }
     }
 }
