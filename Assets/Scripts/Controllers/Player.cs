c02a752335bcb0a98e91a9ceeafab210ad3a7ac3
TheSuperfon <astefanadon@gmail.com>
2024-09-23 20:03:22 -0400

    Task1B deceleration
    deceleration implemented

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour
     public float speed = 10f;
     public float MaxSpeed = 40f;
     public float AccelerationTime = 0;
+    public float DecelerationTime = 2f;
+    public Vector3 PlayerDirection;
 
     void Update()
     {
@@ -28,21 +30,27 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += ((Vector3.up * speed) * Time.deltaTime);
+            PlayerDirection = Vector3.up;
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += ((Vector3.down * speed * Time.deltaTime));
+            PlayerDirection = Vector3.down;
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += ((Vector3.left * speed * Time.deltaTime));
+            PlayerDirection = Vector3.left;
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += ((Vector3.right * speed * Time.deltaTime));
+            PlayerDirection = Vector3.right;
         }
         if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.D)))
         {
+            
+            DecelerationTime = 1f;
             AccelerationTime += Time.deltaTime;
             if (AccelerationTime < 3)
             {
@@ -57,8 +65,24 @@ public class Player : MonoBehaviour
         }
         else
         {
-            speed = 10f;
-            AccelerationTime = 0;
+            DecelerationTime -= Time.deltaTime;
+            if (DecelerationTime > 0)
+            {
+                transform.position += ((PlayerDirection * speed) * Time.deltaTime);
+                if (speed > 0)
+                {
+                    speed -= ((MaxSpeed * Time.deltaTime) / 3);
+                }
+                
+            }
+            else
+            {
+                DecelerationTime = 0;
+                speed = 10f;
+                AccelerationTime = 0;
+            }
+            
+            
         }
     }
 
